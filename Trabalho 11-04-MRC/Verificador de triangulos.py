399e3e3eeea315546f2d4b0e0f9ab639b50680ff
Marcello-MRC <166552467+Marcello-MRC@users.noreply.github.com>
2024-04-16 14:12:22 -0300

    Update Verificador de triangulos.py

--- Trabalho 11-04-MRC/Verificador de triangulos.py
+++ Trabalho 11-04-MRC/Verificador de triangulos.py
@@ -1,8 +1,9 @@
+#Verificador de triangulo
+
 lado1 = input("Primeiro lado: ")
 lado2 = input("Segundo lado: ")
 lado3 = input("Terceiro lado: ")
 
-#Verificando se é um triangulo
 
 if (lado1 + lado2 <lado3) or (lado1 + lado3 < lado2) or (lado2 + lado3 < lado1):
     print("Não e um triangulo")
