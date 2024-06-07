338b3770543a47d2f1e9fb3d4574f3f6bc2ff97c
Marcello-MRC <166552467+Marcello-MRC@users.noreply.github.com>
2024-06-07 15:24:59 -0300

    Add files via upload

---
+++ transformando lista e conjunto.py
@@ -0,0 +1,13 @@
+#lista logo abaixo
+gojo = ["muryo kusho", "infinito", "seis olhos", "mugen", "mugen", "perna1", "perna2"]
+print(gojo)
+
+print("--------------------------------------------------------------------------------")
+#transformando lista em conjuto e tirando os iguais do conjunto
+gojoSet = set(gojo)
+print(gojoSet)
+
+print("--------------------------------------------------------------------------------")
+#trasnformando em conjunto em lista
+gojolist = list(gojoSet)
+print(gojolist)
