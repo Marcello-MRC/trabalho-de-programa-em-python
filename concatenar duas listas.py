fcaccf94d110f4325765b1eb20865a34674f8a50
Marcello-MRC <166552467+Marcello-MRC@users.noreply.github.com>
2024-06-25 16:29:20 -0300

    Update concatenar duas listas.py

--- concatenar duas listas.py
+++ concatenar duas listas.py
@@ -1,1 +1,6 @@
-
+lista1 = []
+lista2 = []
+lista1.append(input("Digite o que voce quer nesta lista: "))
+lista2.append(input("Digite o que voce quer na lista2: "))
+lista_junta = lista1 + lista2
+print(lista_junta)
