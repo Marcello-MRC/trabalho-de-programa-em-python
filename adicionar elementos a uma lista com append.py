f250b3e5a7f1b89717fb153d1e48bec573f8d18a
Marcello-MRC <166552467+Marcello-MRC@users.noreply.github.com>
2024-06-25 16:58:00 -0300

    Update adicionar elementos a uma lista com append.py

--- adicionar elementos a uma lista com append.py
+++ adicionar elementos a uma lista com append.py
@@ -5,7 +5,6 @@ adicionar = input("Adicione elemnetos na lista: ")
 while adicionar != "Parar":        
         adicionar = input("Adicione elemnetos na lista: ")
         lista.append(adicionar)
-        
-        
+      
 lista.pop()
 print(lista)
