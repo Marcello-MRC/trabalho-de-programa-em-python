1a44f5f365c45c2653acf1bb386ebcc8f3f17662
Marcello-MRC <166552467+Marcello-MRC@users.noreply.github.com>
2024-06-25 16:54:39 -0300

    Update tirando duplicadas da lista.py

--- tirando duplicadas da lista.py
+++ tirando duplicadas da lista.py
@@ -1,7 +1,4 @@
-lista_duplicadas = [0,1,2,3,3,3]
-print(lista_duplicadas, "Com duplicadas")
-lista_duplicadas.pop(5)
-lista_duplicadas.pop(4)
-lista = lista_duplicadas
-print(lista, "lista sem duplicadas")
-
+lista_duplicadas = [1,3,3,3,3,0,0,0,0,3,42]
+lista2 = set(lista_duplicadas)
+lista_sem_dup = list(lista2)
+print(lista2)
