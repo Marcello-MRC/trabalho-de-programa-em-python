5b1a1cfa42c585243891aef9027b783594a5d2e2
Marcello-MRC <166552467+Marcello-MRC@users.noreply.github.com>
2024-06-25 15:22:29 -0300

    Add files via upload

---
+++ tirando duplicadas da lista.py
@@ -0,0 +1,7 @@
+lista_duplicadas = [0,1,2,3,3,3]
+print(lista_duplicadas, "Com duplicadas")
+lista_duplicadas.pop(5)
+lista_duplicadas.pop(4)
+lista = lista_duplicadas
+print(lista, "lista sem duplicadas")
+
