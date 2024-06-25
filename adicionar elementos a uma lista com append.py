2fa0b94e8521fcc342c3a9ab21c1a8e7984e6a46
Marcello-MRC <166552467+Marcello-MRC@users.noreply.github.com>
2024-06-25 16:47:19 -0300

    Add files via upload

---
+++ adicionar elementos a uma lista com append.py
@@ -0,0 +1,11 @@
+lista = []
+
+adicionar = input("Adicione elemnetos na lista: ")
+
+while adicionar != "Parar":        
+        adicionar = input("Adicione elemnetos na lista: ")
+        lista.append(adicionar)
+        
+        
+lista.pop()
+print(lista)
