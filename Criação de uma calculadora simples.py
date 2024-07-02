f566b76b617de87b898bc30958dce9924c9e3ae6
Marcello-MRC <166552467+Marcello-MRC@users.noreply.github.com>
2024-07-02 15:34:48 -0300

    Add files via upload

---
+++ Criação de uma calculadora simples.py
@@ -0,0 +1,22 @@
+n1 = int(input("Digite um numero: "))
+n2 = int(input("Digite outro numero: "))
+print("----------------------------------")
+#Adição
+print("ADIÇÃO")
+print("{} + {}".format(n1, n2))
+print(n1 + n2)
+print("----------------------------------")
+#SUBTRAÇÃO
+print("SUBTRAÇÃO")
+print("{} - {}".format(n1, n2))
+print(n1 - n2)
+print("----------------------------------")
+#MULTIPLICAÇÃO
+print("MULTIPLICAÇÃO")
+print("{} * {}".format(n1, n2))
+print(n1 * n2)
+print("----------------------------------")
+#DIVISÃO
+print("DIVISÃO")
+print("{} / {}".format(n1, n2))
+print(n1 / n2)
