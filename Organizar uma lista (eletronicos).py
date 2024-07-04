119d01c613ef738d25dcfbefa870bf238cd9d006
Marcello-MRC <166552467+Marcello-MRC@users.noreply.github.com>
2024-07-04 16:46:04 -0300

    Add files via upload

---
+++ Organizar uma lista (eletronicos).py
@@ -0,0 +1,60 @@
+comidas = ['leite', 'couve','computador', 'tomate','garfo','faca','tablet','refrigerante']
+bebidas = ['uva', 'colher','TV','vinho','cerveja','celular','banana']
+talheres = ['arroz','iPhone', 'concha','whisky','vodka','feijão','colher de chá']
+eletronicos = []
+#----------------------------------------------
+#comidas #remove
+comidas.remove("computador")
+comidas.remove("tablet")
+comidas.remove("garfo")
+comidas.remove("faca")
+comidas.remove("refrigerante")
+comidas.remove("leite")
+#---------------------------------------------
+#comidas #append
+comidas.append("uva")
+comidas.append("banana")
+comidas.append("arroz")
+comidas.append("feijão")
+#----------------------------------------------
+#bebidas #remove
+bebidas.remove("colher")
+bebidas.remove("TV")
+bebidas.remove("celular")
+bebidas.remove("uva")
+bebidas.remove("banana")
+#------------------------------------------------
+#bebidas #append
+bebidas.append("leite")
+bebidas.append("refrigerante")
+bebidas.append("whisky")
+bebidas.append("vodka")
+#------------------------------------------------
+#talheres #remove
+talheres.remove("arroz")
+talheres.remove("iPhone")
+talheres.remove("whisky")
+talheres.remove("vodka")
+talheres.remove("feijão")
+#------------------------------------------------
+#talheres #append
+talheres.append("garfo")
+talheres.append("faca")
+talheres.append("colher")
+#-------------------------------------------------
+#eletronicos #append
+eletronicos.append("computador")
+eletronicos.append("tablet")
+eletronicos.append("TV")
+eletronicos.append("celular")
+eletronicos.append("iphone")
+#-------------------------------------------------
+print("comidas ->", comidas)
+print("bebidas ->", bebidas)
+print("talheres ->" ,talheres)
+print("eletronicos ->", eletronicos)
+
+
+
+
+
